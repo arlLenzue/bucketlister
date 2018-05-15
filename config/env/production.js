248964be5fa7f2c88c295a75516ac6e1efada7ac
arlLenzue <39239552+arlLenzue@users.noreply.github.com>
2018-05-15 22:22:39 +0800

    Add files via upload

---
+++ config/env/production.js
@@ -0,0 +1,10 @@
+module.exports = { 
+	db: process.env.MONGODB_URI || 'mongodb://localhost/bucketlist',
+	sessionSecret: 'developmentSessionSecret',
+	facebook: { 
+		clientID: process.env.ClientID, 
+		clientSecret: process.env.ClientSecret,
+		callbackURL: 'https://bucketlister-production.herokuapp.com/oauth/facebook/callback' 
+	}
+
+};
