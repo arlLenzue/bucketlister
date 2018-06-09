da900b8c048f1ac0dfcf98f4326f196b2f4add96
arlLenzue <39239552+arlLenzue@users.noreply.github.com>
2018-06-09 22:16:24 +0800

    Add files via upload

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,10 +2,11 @@ var config = require('./config'),
 	mongoose = require('mongoose'); 
 
 module.exports = function() {
+	
 	var db = mongoose.connect(config.db); 
 
 	require('../app/models/user.server.model'); 
-	
+
 	require('../app/models/bucketitem.server.model'); 
 	
 	require('../app/models/buckettemplate.server.model'); 
